f21eac28aae977eda6ff8b2659b3456dd874b961
TheLudway <70478991+TheLudway@users.noreply.github.com>
2023-02-12 16:21:02 -0500

    DOCUMENTATION AND SIGNATURE

--- SIMPSON-EXCEL.py
+++ SIMPSON-EXCEL.py
@@ -5,7 +5,7 @@ wb = load_workbook('SPEED.xlsx')
 ws = wb.active
 Resultado_suma = 0
 
-#LAS CELDAS QUE SE MULTIPLICAN POR (4) SON LAS IMPARES Y LAS QUE SE MULTIPLICAN POR (2) SON LAS PARES, SE HACE UNA VERIFICACIÓN SI LA CELDA ES PAR O IMPAR 
+#Odd cells get multyply by 4 and even cells get multiply by 2.
 for row in range(3, 40981):
     if row % 2 == 1:
         for col in [2]:
@@ -16,11 +16,21 @@ for row in range(3, 40981):
             char = get_column_letter(col)
             Resultado_suma += (ws[char + str(row)].value) * 2
 
-#AGREGAR LOS PRIMEROS VALORES A LA SUMA INTERIOR
+#Sum first and last cells
 Resultado_suma += ws["B2"].value + ws["B40981"].value
 
-#OBTENER LOS LÍMITES DE INTEGRACIÓN Y CÀLCULAR DELTA DE X
+#Get integration limits and get delta x
 Delta_x = (ws["A40981"].value - ws["A2"].value) / 40980
 
 Resultado_final = (Resultado_suma * Delta_x) / 3
 print(Resultado_final)
+ 
+"""
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣶⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣄⣀⡀⣠⣾⡇⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⢿⣿⣿⡇⠀⠀⠀⠀
+⠀⣶⣿⣦⣜⣿⣿⣿⡟⠻⣿⣿⣿⣿⣿⣿⣿⡿⢿⡏⣴⣺⣦⣙⣿⣷⣄⠀⠀⠀
+⠀⣯⡇⣻⣿⣿⣿⣿⣷⣾⣿⣬⣥⣭⣽⣿⣿⣧⣼⡇⣯⣇⣹⣿⣿⣿⣿⣧⠀⠀
+⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠸⣿⣿⣿⣿⣿⣿⣿⣷⠀
+""" 
